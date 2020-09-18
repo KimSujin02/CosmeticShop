b3ee0203ad44725b8c040aa0a3350def1cd8261d
sujin <selina9135@gmail.com>
2020-09-18 15:42:12 +0900

    .

--- cart/views.py
+++ cart/views.py
@@ -29,4 +29,4 @@ def detail(request):
     cart = Cart(request)
     for product in cart:
         product['quantity_form'] = AddProductForm(initial={'quantity' : product['quantity'], 'is_update' : True})
-    return render(request, 'cart/detail.html', {'cart':cart})
+    return render(request, 'cart/detail.html', {'cart': cart})
